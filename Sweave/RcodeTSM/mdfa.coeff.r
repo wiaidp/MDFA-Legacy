7e68a68e2e38ef1e7a6d90fd587e4d54c6f75bd3
tuckermcelroy <neidev1@yahoo.com>
2019-05-22 17:34:34 +0200

    chap 9 updates
    continuing LLM mixed freq example

--- Sweave/RcodeTSM/mdfa.coeff.r
+++ Sweave/RcodeTSM/mdfa.coeff.r
@@ -9,7 +9,7 @@ mdfa.coeff <- function(frf,aft,fore)
 	#		frequency response function (frf), for indices
 	#		aft through fore
 	#	inputs:
-	#		frf is array N x N x Grid of complex entries
+	#		frf is array N x N x grid of complex entries
   	#		aft <= fore are integer indices
 	#	outputs:
 	#		filter is array N x N x (fore-aft+1) of real entries
@@ -17,15 +17,15 @@ mdfa.coeff <- function(frf,aft,fore)
 	################################################# 
 
 	N <- dim(frf)[1]
-	Grid <- dim(frf)[3]
-	m <- floor(Grid/2)
-	lambda.ft <- exp(-1i*2*pi*Grid^{-1}*(seq(1,Grid) - (m+1)))	## this is e^{-i lambda}
+	grid <- dim(frf)[3]
+	m <- floor(grid/2)
+	lambda.ft <- exp(-1i*2*pi*grid^{-1}*(seq(1,grid) - (m+1)))	## this is e^{-i lambda}
 	filter <- array(0,c(N,N,(fore-aft+1)))
 
 	for(l in aft:fore)
 	{
-		val <- do.call(cbind,lapply(seq(1,Grid),function(i) frf[,,i] * lambda.ft[i]^{-l}))
-		val <- Grid^{-1}*val %*% (rep(1,Grid) %x% diag(N))
+		val <- do.call(cbind,lapply(seq(1,grid),function(i) frf[,,i] * lambda.ft[i]^{-l}))
+		val <- grid^{-1}*val %*% (rep(1,grid) %x% diag(N))
 		filter[,,l-aft+1] <- Re(val)
 	}
 
