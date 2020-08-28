2890ee183bab54a7e6494b9b04bbec1d96ef5635
Tucker McElroy <33321700+tuckermcelroy@users.noreply.github.com>
2020-08-28 17:28:26 -0400

    Update generate_MDFA_Legacy.r
    updating from august changes

--- Sweave/Rnw/generate_MDFA_Legacy.r
+++ Sweave/Rnw/generate_MDFA_Legacy.r
@@ -3,7 +3,6 @@
 rm(list=ls())
 
 
-
 library(xts)
 # State-space models (will be replicated by MDFA) 
 library(dlm)
@@ -22,19 +21,18 @@ library(stringr)  # For str_trim() to trip whitespace from strings
 library(Quandl)
 require (Quandl)
 #Quandl.api_key("ivVdJGV57TXA1RX5jgvp")
-
-# Set paths
-#path.main <- "C:\\Users\\Tucker\\Documents\\GitHub\\MDFA-Legacy"
-#path.pgm <- paste(path.main,"Rnw\\",sep="")
-#path.out <- paste(path.main,"Latex\\",sep="")
+#  Tucker's libraries
+library(expm)
+library(R.utils)
+library(xtable)
 
 # set directory to GitHub/MDFA-Legacy
-setwd("C:\\Users\\Tucker\\Documents\\GitHub\\MDFA-Legacy")
-path.main<-paste(getwd(),"/Sweave/",sep="")
-path.pgm<-paste(path.main,"Rnw/",sep="")
-path.out<-paste(path.main,"Latex/",sep="")
-
-
+path.main <- paste(getwd(),"/Sweave/",sep="")
+path.pgm <- paste(path.main,"Rnw/",sep="")
+path.out <- paste(path.main,"Latex/",sep="")
+ 
+## Load MDFA R codes #
+sourceDirectory(path=paste(path.main,"RcodeTSM/",sep=""), modifiedOnly=FALSE)
 
 script <- paste(path.pgm,"MDFA_Legacy",sep="")
 
